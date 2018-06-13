9c3ac608198070c2e8a82eef235d7fe86cb0fa98
GonnX <peterp154629@gmail.com>
2018-06-13 13:27:03 +0800

    加了graph

--- app/src/main/java/com/example/luolab/acquisition_platform/GsrView.java
+++ app/src/main/java/com/example/luolab/acquisition_platform/GsrView.java
@@ -18,8 +18,6 @@ import android.os.Message;
 import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.support.transition.Slide;
-import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Gravity;
@@ -33,7 +31,12 @@ import android.widget.Toast;
 
 import com.physicaloid.lib.Physicaloid;
 import com.physicaloid.lib.usb.driver.uart.ReadLisener;
-import com.physicaloid.lib.usb.driver.uart.UartConfig;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -42,6 +45,7 @@ public class GsrView extends Fragment{
 
     private Button startBtn;
     private Button backBtn;
+    private Button nextBtn;
     private Button setSampleRateBtn;
 
     private ImageView acupointImage;
@@ -49,7 +53,6 @@ public class GsrView extends Fragment{
 
     private Object[] imageResource;
     private Physicaloid mPhysicaloid;
-    private TextView tv;
 
     private boolean startFlag;
 
@@ -58,6 +61,7 @@ public class GsrView extends Fragment{
     private String[] sampleRate_Item;
     private String selectSampleRate;
 
+    private Handler Graph_Handle;
     private Handler measureAcupoint_Handler;
     private Handler TimeDialog_Timer_Handler;
     private Handler mHandler;
@@ -72,21 +76,35 @@ public class GsrView extends Fragment{
 
     private View dialogView;
 
+    private GraphView G_Graph;
+    private LineGraphSeries<DataPoint> G_Series;
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         gsrView = inflater.inflate(R.layout.gsr, container, false);
+
+        G_Graph = gsrView.findViewById(R.id.data_chart);
+
+        ResetGraph();
+
+        G_Graph.post(new Runnable() {
+            @Override
+            public void run() {
+                G_Series.appendData(new DataPoint(10,250),true,50);
+                G_Graph.getViewport().setMaxX(50);
+            }
+        });
 
         acupointImage = gsrView.findViewById(R.id.Gsr_ImageView);
 
         imageResource = new Object[]{R.drawable.one,R.drawable.two,R.drawable.three,
                                      R.drawable.four,R.drawable.five,R.drawable.six};
 
-        tv = gsrView.findViewById(R.id.textView);
-
         mHandler = new Handler();
         measureAcupoint_Handler = new Handler();
         TimeDialog_Timer_Handler = new Handler();
+        Graph_Handle = new Handler();
 
 
         dialogView = View.inflate(inflater.getContext(),R.layout.dialog,null);
@@ -117,19 +135,26 @@ public class GsrView extends Fragment{
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onClick(View view) {
-                ((Vibrator) inflater.getContext().getSystemService(Service.VIBRATOR_SERVICE)).vibrate(new long[]{0,50}, -1);
+                StartBtn_Click(inflater);
+//                timer_updateImage = new Timer();
+//
+//                timerTask_updateImage = new TimerTask() {
+//                    @Override
+//                    public void run() {
+//
+//                        UpdateAcupointImage();
+//                        DialogShow(inflater);
+//                    }
+//                };
+//                timer_updateImage.schedule(timerTask_updateImage,0,4000);
+            }
+        });
 
-                timer_updateImage = new Timer();
-
-                timerTask_updateImage = new TimerTask() {
-                    @Override
-                    public void run() {
-
-                        UpdateAcupointImage();
-                        DialogShow(inflater);
-                    }
-                };
-                timer_updateImage.schedule(timerTask_updateImage,0,4000);
+        nextBtn = gsrView.findViewById(R.id.next_btn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NextBtn_Click(inflater);
             }
         });
 
@@ -137,7 +162,7 @@ public class GsrView extends Fragment{
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                BackBtn_Click(inflater);
             }
         });
 
@@ -174,88 +199,109 @@ public class GsrView extends Fragment{
 
         return gsrView;
     }
-    private void UpdateAcupointImage(){
+    private void ResetGraph()
+    {
+        G_Graph.getViewport().setMaxX(1);
+        G_Graph.getViewport().setMaxY(1023);
+        G_Graph.getViewport().setYAxisBoundsManual(true);
 
+        G_Graph.getGridLabelRenderer().reloadStyles();
+
+        G_Graph.removeAllSeries();
+        G_Series = new LineGraphSeries<DataPoint>();
+        G_Graph.addSeries(G_Series);
+    }
+    private void UpdateAcupointImage(final int state, final int index){
         measureAcupoint_Handler.post(new Runnable() {
             @Override
             public void run() {
-                acupointImage.setBackgroundResource((Integer) imageResource[counter++]);
-                measureAcupoint_Handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        byte[] buf = new byte[1];
-
-                        if(selectSampleRate.equals(sampleRate_Item[0]))
-                            buf[0] = 0x30;
-                        else if(selectSampleRate.equals(sampleRate_Item[1]))
-                            buf[0] = 0x31;
-                        else
-                            buf[0] = 0x32;
-
-                        mPhysicaloid.write(buf, buf.length);
-                    }
-                },3000);
-                if(counter == 6) {
-                    counter = 0;
-                    timer_updateImage.cancel();
-                    timer_updateImage = null;
-
-                    timerTask_updateImage.cancel();
-                    timerTask_updateImage = null;
+                switch(state) {
+                    case 1:
+                        acupointImage.setBackgroundResource((Integer) imageResource[index]);
+                        break;
+                    case 2:
+                        acupointImage.setBackgroundResource((Integer) imageResource[index]);
+                        break;
+                    default:
+                        break;
                 }
             }
         });
     }
-    private void DialogShow(final LayoutInflater inflater){
-        TimeDialog_Timer_Handler.post(new Runnable() {
-            @Override
-            public void run() {
-                TimeDialog_Timer = new CountDownTimer(3000,1000){
-                    @Override
-                    public void onTick(final long millisUntilFinished) {
-                        TimeDialog_Timer_Handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                TextView tv = TimeDialog.findViewById(R.id.dialog_tv);
-                                tv.setText(new String(String.valueOf(millisUntilFinished / 1000)));
-                            }
-                        });
-                        TimeDialog.show();
-                    }
-                    @Override
-                    public void onFinish() {
-                        TimeDialog.dismiss();
-                    }
-                };
-                TimeDialog_Timer.start();
-            }
-        });
-    }
-
-    private void tvAppend(TextView tv, CharSequence text) {
-        final TextView ftv = tv;
-        final CharSequence ftext = text;
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                ftv.append(ftext);
-            }
-        });
-    }
+//    private void UpdateAcupointImage(){
+//
+//        measureAcupoint_Handler.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                acupointImage.setBackgroundResource((Integer) imageResource[counter++]);
+//                measureAcupoint_Handler.postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        byte[] buf = new byte[1];
+//
+//                        if(selectSampleRate.equals(sampleRate_Item[0]))
+//                            buf[0] = 0x30;
+//                        else if(selectSampleRate.equals(sampleRate_Item[1]))
+//                            buf[0] = 0x31;
+//                        else
+//                            buf[0] = 0x32;
+//
+//                        mPhysicaloid.write(buf, buf.length);
+//                    }
+//                },4000);
+//                if(counter == 6) {
+//                    counter = 0;
+//                    timer_updateImage.cancel();
+//                    timer_updateImage = null;
+//
+//                    timerTask_updateImage.cancel();
+//                    timerTask_updateImage = null;
+//                }
+//            }
+//        });
+//    }
+//    private void DialogShow(final LayoutInflater inflater){
+//        TimeDialog_Timer_Handler.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                TimeDialog_Timer = new CountDownTimer(4000,1000){
+//                    @Override
+//                    public void onTick(final long millisUntilFinished) {
+//                        TimeDialog_Timer_Handler.post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                TextView tv = TimeDialog.findViewById(R.id.dialog_tv);
+//                                tv.setText(new String(String.valueOf(millisUntilFinished / 1000)));
+//                            }
+//                        });
+//                        TimeDialog.show();
+//                    }
+//                    @Override
+//                    public void onFinish() {
+//                        TimeDialog.dismiss();
+//                    }
+//                };
+//                TimeDialog_Timer.start();
+//            }
+//        });
+//    }
     private void setEnabledUi(int state){
         if(state == 0){
             startBtn.setEnabled(true);
+            nextBtn.setEnabled(true);
             backBtn.setEnabled(true);
             setSampleRateBtn.setEnabled(true);
         }
         else if(state == 1){
             startBtn.setEnabled(false);
             backBtn.setEnabled(false);
+            nextBtn.setEnabled(false);
             setSampleRateBtn.setEnabled(true);
         }
         else if(state == 2){
             startBtn.setEnabled(false);
             backBtn.setEnabled(false);
+            nextBtn.setEnabled(false);
             setSampleRateBtn.setEnabled(false);
         }
         else if(state == 3){
@@ -267,15 +313,51 @@ public class GsrView extends Fragment{
     private void setSerialOpen(final LayoutInflater inflater) {
         if (mPhysicaloid.open()) {
             Toast.makeText(inflater.getContext(), "open", Toast.LENGTH_SHORT).show();
-            tv.setMovementMethod(new ScrollingMovementMethod());
             mPhysicaloid.addReadListener(new ReadLisener() {
                 @Override
                 public void onRead(int i) {
                     byte[] buf = new byte[i];
                     mPhysicaloid.read(buf, i);
-                    tvAppend(tv,new String(buf));
+                    SetChoice(buf,i,inflater);
                 }
             });
         }
+    }
+    private void SetChoice(byte[] buf,int size,LayoutInflater inflater){
+        if(size == 1){
+            if(buf[0] == '0')
+                BackBtn_Click(inflater);
+            else if(buf[0] == '1')
+                StartBtn_Click(inflater);
+            else
+                NextBtn_Click(inflater);
+        }
+    }
+
+    private void StartBtn_Click(LayoutInflater inflater){
+        ((Vibrator) inflater.getContext().getSystemService(Service.VIBRATOR_SERVICE)).vibrate(new long[]{0,50}, -1);
+        byte[] Data = new byte[1];
+
+        if(selectSampleRate.equals(sampleRate_Item[0]))
+            Data[0] = 0x30;
+        else if(selectSampleRate.equals(sampleRate_Item[1]))
+            Data[0] = 0x31;
+        else
+            Data[0] = 0x32;
+        mPhysicaloid.write(Data, Data.length);
+    }
+    private void NextBtn_Click(LayoutInflater inflater){
+        ((Vibrator) inflater.getContext().getSystemService(Service.VIBRATOR_SERVICE)).vibrate(new long[]{0,50}, -1);
+        if(counter == 5)
+            UpdateAcupointImage(1,5);
+        else
+            UpdateAcupointImage(1,++counter);
+    }
+    private void BackBtn_Click(LayoutInflater inflater){
+        ((Vibrator) inflater.getContext().getSystemService(Service.VIBRATOR_SERVICE)).vibrate(new long[]{0,50}, -1);
+        if(counter == 0)
+            UpdateAcupointImage(2,0);
+        else
+            UpdateAcupointImage(2,--counter);
     }
 }
